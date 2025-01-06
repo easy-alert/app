72d31405974e9d40e246959a41863e482061e0a6
Augusto Mandelli <mandelli.augusto@gmail.com>
2025-01-06 14:51:55 -0300

    fix: Info Plist

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseApi = axios.create({
   baseURL:
     process.env.EXPO_PUBLIC_API_URL ||
-    "https://easyalert-sandbox.herokuapp.com/api",
+    "https://easyalert-production.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
