789b2f3f47b2cccce89f09935d6b3b4957b5d4eb
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-05 17:48:29 -0300

    feat: add request/response interceptors to baseApi for authentication and error handling

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -1,9 +1,34 @@
 import axios from "axios";
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export const baseApi = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080/api',
+  
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
+
+
+baseApi.interceptors.request.use(
+  async (config: any) => {
+    const token = await AsyncStorage.getItem('authToken');
+
+    if (token) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    console.error('Request error:', error);
+    return Promise.reject(error);
+  },
+);
+
+baseApi.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error),
+);
