06ea988a3c77d2298215cb5934fb32a1422d1278
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 13:26:48 -0300

    feat: implement caching for getMaintenanceHistorySupplier and remove getSuppliersByMaintenanceId service

--- src/services/getMaintenanceHistorySupplier.ts
+++ src/services/getMaintenanceHistorySupplier.ts
@@ -1,3 +1,5 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { baseApi } from './baseApi';
 
 interface IGetMaintenanceHistorySupplier {
@@ -12,8 +14,26 @@ export async function getMaintenanceHistorySupplier ({
     try {
       const response = await baseApi.get(uri);
 
+      await AsyncStorage.setItem(uri, JSON.stringify(response.data));
+
+
       return response.data;
     } catch (error) {
-      console.log("🚀 ~ error:", error)
+      console.error(
+        "Erro ao buscar os dados ou sem internet, carregando do cache:",
+        error
+      );
+  
+      try {
+        const cachedData = await AsyncStorage.getItem(uri);
+  
+        if (cachedData) {
+          return JSON.parse(cachedData);
+        }
+      } catch (cacheError) {
+        console.error("Erro ao carregar dados do cache:", cacheError);
+      }
+  
+      return null
     }
 };
