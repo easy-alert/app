4067ddea52f35a8c3f2523912b5c075a0aa5ed70
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 13:27:47 -0300

    feat: update uploadFile API endpoint for company file uploads

--- src/services/uploadFile/index.ts
+++ src/services/uploadFile/index.ts
@@ -14,7 +14,7 @@ export const uploadFile = async (file: {
     } as any);
 
     const response = await fetch(
-      `https://easyalert-production.herokuapp.com/api/client/upload/file`,
+      `https://easyalert-production.herokuapp.com/api/company/upload/file`,
       {
         method: "POST",
         headers: {
