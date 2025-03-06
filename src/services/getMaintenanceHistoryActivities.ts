0ad5fb82924a861cd60aa5378f4c346f9b7de51f
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 13:22:03 -0300

    feat: remove deprecated getHistoryActivitiesFromMaintenance service and update getMaintenanceHistoryActivities for improved caching

--- src/services/getMaintenanceHistoryActivities.ts
+++ src/services/getMaintenanceHistoryActivities.ts
@@ -1,21 +1,38 @@
-import { baseApi } from './baseApi';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { baseApi } from "./baseApi";
 
 interface IGetMaintenanceHistoryActivities {
   maintenanceHistoryId: string;
 }
 
 export async function getMaintenanceHistoryActivities({
-  maintenanceHistoryId 
+  maintenanceHistoryId,
 }: IGetMaintenanceHistoryActivities) {
-  const uri= `company/maintenance-history-activities/${maintenanceHistoryId}` 
+  const uri = `company/maintenance-history-activities/${maintenanceHistoryId}`;
 
   try {
     const response = await baseApi.get(uri);
 
+    await AsyncStorage.setItem(uri, JSON.stringify(response.data));
+
     return response.data;
   } catch (error) {
-    console.log("🚀 ~ error:", error)
+    console.error(
+      "Erro ao buscar os dados ou sem internet, carregando do cache:",
+      error
+    );
 
-    return {};
+    try {
+      const cachedData = await AsyncStorage.getItem(uri);
+
+      if (cachedData) {
+        return JSON.parse(cachedData);
+      }
+    } catch (cacheError) {
+      console.error("Erro ao carregar dados do cache:", cacheError);
+    }
+
+    return null; // Retorna null se nenhum dado for encontrado
   }
-};
+}
