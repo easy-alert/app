45e7b3a7e740b019bda75d444588f83bd793ca01
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 17:07:06 -0300

    feat: refactor getMaintenanceHistorySupplier for improved error handling and code clarity

--- src/services/getMaintenanceHistorySupplier.ts
+++ src/services/getMaintenanceHistorySupplier.ts
@@ -1,39 +1,38 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { baseApi } from './baseApi';
+import { baseApi } from "./baseApi";
 
 interface IGetMaintenanceHistorySupplier {
   maintenanceHistoryId: string;
 }
 
-export async function getMaintenanceHistorySupplier ({
-  maintenanceHistoryId
-  }: IGetMaintenanceHistorySupplier) {
-    const uri = `company/suppliers/selected/${maintenanceHistoryId}`;
+export async function getMaintenanceHistorySupplier({
+  maintenanceHistoryId,
+}: IGetMaintenanceHistorySupplier) {
+  const uri = `company/suppliers/selected/${maintenanceHistoryId}`;
+
+  try {
+    const response = await baseApi.get(uri);
+
+    await AsyncStorage.setItem(uri, JSON.stringify(response.data));
+
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Erro ao buscar os dados ou sem internet, carregando do cache:",
+      error
+    );
 
     try {
-      const response = await baseApi.get(uri);
+      const cachedData = await AsyncStorage.getItem(uri);
 
-      await AsyncStorage.setItem(uri, JSON.stringify(response.data));
-
-
-      return response.data;
-    } catch (error) {
-      console.error(
-        "Erro ao buscar os dados ou sem internet, carregando do cache:",
-        error
-      );
-  
-      try {
-        const cachedData = await AsyncStorage.getItem(uri);
-  
-        if (cachedData) {
-          return JSON.parse(cachedData);
-        }
-      } catch (cacheError) {
-        console.error("Erro ao carregar dados do cache:", cacheError);
+      if (cachedData) {
+        return JSON.parse(cachedData);
       }
-  
-      return null
+    } catch (cacheError) {
+      console.error("Erro ao carregar dados do cache:", cacheError);
     }
-};
+
+    return null;
+  }
+}
