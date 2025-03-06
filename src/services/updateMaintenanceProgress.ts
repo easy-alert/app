f0aaeb9d3423c3664d673e21004e17441e014b08
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 13:27:05 -0300

    feat: remove saveProgressInMaintenance service and implement updateMaintenanceProgress for improved maintenance tracking

---
+++ src/services/updateMaintenanceProgress.ts
@@ -0,0 +1,47 @@
+import { baseApi } from "./baseApi";
+
+import { alertMessage, catchHandler } from "../utils/handleAlerts";
+
+import type { IError } from "../types/IError";
+
+interface IUpdateMaintenanceProgress {
+  syndicNanoId: string;
+  userId: string;
+  maintenanceHistoryId: string;
+  inProgressChange: boolean;
+}
+
+export const updateMaintenanceProgress = async ({
+  syndicNanoId,
+  userId,
+  maintenanceHistoryId,
+  inProgressChange,
+}: IUpdateMaintenanceProgress) => {
+  const uri = `company/maintenances/set/in-progress`;
+
+  const params = {
+    syndicNanoId,
+  };
+
+  const body = {
+    userId,
+    maintenanceHistoryId,
+    inProgressChange,
+  };
+
+  try {
+    const response = await baseApi.post(uri, body, { params });
+
+    alertMessage({
+      type: "success",
+      message: response?.data?.ServerMessage?.message,
+    });
+  } catch (error: any) {
+    const response = error.response as IError;
+
+    catchHandler({
+      message: response?.data?.ServerMessage?.message,
+      statusCode: response?.status,
+    });
+  }
+};
