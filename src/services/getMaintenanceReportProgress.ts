7d81430fb56a8656f154b29b0dff36a3eff038fc
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 17:07:11 -0300

    feat: enhance getMaintenanceReportProgress with caching and improved error handling

--- src/services/getMaintenanceReportProgress.ts
+++ src/services/getMaintenanceReportProgress.ts
@@ -1,18 +1,38 @@
-import { baseApi } from './baseApi';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { baseApi } from "./baseApi";
 
 export interface IGetMaintenanceReportProgress {
   maintenanceHistoryId: string;
 }
 
-export const getMaintenanceReportProgress = async ({ maintenanceHistoryId }: IGetMaintenanceReportProgress) => {
+export const getMaintenanceReportProgress = async ({
+  maintenanceHistoryId,
+}: IGetMaintenanceReportProgress) => {
   const uri = `company/maintenances/list/report/progress/${maintenanceHistoryId}`;
 
   try {
     const response = await baseApi.get(uri);
 
+    await AsyncStorage.setItem(uri, JSON.stringify(response.data));
+
     return response.data;
   } catch (error: any) {
-    console.log("🚀 ~ getMaintenanceReportProgress ~ error:", error)
-    return {};
+    console.error(
+      "Erro ao buscar os dados ou sem internet, carregando do cache:",
+      error
+    );
+
+    try {
+      const cachedData = await AsyncStorage.getItem(uri);
+
+      if (cachedData) {
+        return JSON.parse(cachedData);
+      }
+    } catch (cacheError) {
+      console.error("Erro ao carregar dados do cache:", cacheError);
+    }
+
+    return null;
   }
 };
