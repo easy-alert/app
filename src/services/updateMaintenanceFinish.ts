665fc3bcad7a2da158920900a098c153c62c754b
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-06 13:18:41 -0300

    feat: replace finishMaintenance service with updateMaintenanceFinish for improved maintenance reporting

---
+++ src/services/updateMaintenanceFinish.ts
@@ -0,0 +1,56 @@
+import { baseApi } from "./baseApi";
+
+import { alertMessage, catchHandler } from "../utils/handleAlerts";
+
+import type { IMaintenanceReportProgress } from "../types/IMaintenanceReportProgress";
+import type { IAnnexesAndImages } from "../types/IAnnexesAndImages";
+import type{ IError } from "../types/IError";
+
+interface IUpdateMaintenanceFinish {
+  syndicNanoId: string;
+  userId: string;
+  maintenanceHistoryId: string;
+  maintenanceReport: IMaintenanceReportProgress;
+  files: IAnnexesAndImages[];
+  images: IAnnexesAndImages[];
+}
+
+export const updateMaintenanceFinish = async ({
+  syndicNanoId,
+  userId,
+  maintenanceHistoryId,
+  maintenanceReport,
+  files,
+  images,
+}: IUpdateMaintenanceFinish) => {
+  const uri = `company/maintenances/create/report`;
+
+  const body = {
+    userId,
+    responsibleSyndicId: syndicNanoId,
+    maintenanceHistoryId,
+    cost: maintenanceReport.cost,
+    observation:
+      maintenanceReport.observation !== ""
+        ? maintenanceReport.observation
+        : null,
+    ReportAnnexes: files,
+    ReportImages: images,
+  };
+
+  try {
+    const response = await baseApi.post(uri, body);
+
+    alertMessage({
+      type: "success",
+      message: response?.data?.ServerMessage?.message,
+    });
+  } catch (error: any) {
+    const response = error.response as IError;
+
+    catchHandler({
+      message: response?.data?.ServerMessage?.message,
+      statusCode: response?.status,
+    });
+  }
+};
