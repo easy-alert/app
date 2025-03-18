6c21f92260cf93fcc2cd424aa541857b3b52194e
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-03-18 09:23:33 -0300

    refactor: create navbar style file

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
-import {
-  View,
-  Image,
-  StyleSheet,
-  SafeAreaView,
-  Modal,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  Linking,
-  StatusBar,
-  Platform,
-} from "react-native";
+import { View, Image, SafeAreaView, Modal, Text, TouchableOpacity, FlatList, Linking } from "react-native";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Feather";
+
+import { styles } from "./styles";
 
 interface NavbarProps {
   logoUrl: string;
@@ -88,67 +78,3 @@ export const Navbar: React.FC<NavbarProps> = ({ logoUrl, syndicNanoId, buildingN
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  safeArea: {
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  navbar: {
-    width: "100%",
-    height: 60,
-    backgroundColor: "#fff",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  hamburgerIcon: {
-    position: "absolute",
-    left: 16,
-  },
-  logoContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
-  logo: {
-    width: 120,
-    height: 40,
-    resizeMode: "contain",
-  },
-  modalContainer: {
-    flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    justifyContent: "center",
-  },
-  modalContent: {
-    flex: 1,
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    paddingTop: 20,
-    paddingHorizontal: 20,
-  },
-  modalHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginVertical: 20,
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  closeIcon: {
-    padding: 5,
-  },
-  optionItem: {
-    paddingVertical: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  optionText: {
-    fontSize: 16,
-  },
-});
