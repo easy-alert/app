159a3afd843fd9f5b00d2afe1c765282cb26d20c
Augusto Mandelli <augustomandelli@Augustos-MacBook-Air-2.local>
2025-04-03 18:18:02 -0300

    fix: kanban offline

--- src/services/getMaintenancesKanban.ts
+++ src/services/getMaintenancesKanban.ts
@@ -1,3 +1,5 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { baseApi } from "./baseApi";
 
 interface IGetMaintenancesKanban {
@@ -27,13 +29,28 @@ export const getMaintenancesKanban = async ({ userId, filter }: IGetMaintenances
 
   const uri = "/mobile/buildings/maintenances/kanban";
 
+  // Criar uma chave única para o cache com base na URI + parâmetros
+  const cacheKey = `${uri}?${new URLSearchParams(params as any).toString()}`;
+
   try {
     const response = await baseApi.get(uri, { params });
 
+    await AsyncStorage.setItem(cacheKey, JSON.stringify(response.data));
+
     return response.data;
   } catch (error: any) {
-    console.error("Erro ao buscar os dados ou sem internet:", error);
+    console.error("Erro ao buscar os dados ou sem internet, carregando do cache (getMaintenancesKanban):", error);
 
-    return {};
+    try {
+      const cachedData = await AsyncStorage.getItem(cacheKey);
+
+      if (cachedData) {
+        return JSON.parse(cachedData);
+      }
+    } catch (cacheError) {
+      console.error("Erro ao carregar dados do cache:", cacheError);
+    }
+
+    return {}; // Retorna objeto vazio se nada for encontrado
   }
 };
