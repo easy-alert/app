e0abeca8d4239c24fd20f49ff158c195948bfde6
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-04-08 16:24:07 -0300

    chore: add reanimated and gesture handler config

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { StatusBar } from "expo-status-bar";
 
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Routes } from "@/routes";
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView>
       <StatusBar translucent style="dark" />
 
       <SafeAreaProvider>
@@ -20,7 +22,7 @@ export default function App() {
           </AuthProvider>
         </View>
       </SafeAreaProvider>
-    </>
+    </GestureHandlerRootView>
   );
 }
 
