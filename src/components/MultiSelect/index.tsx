42a80cc9ff3b5ad3a72e4d479edc50bbde326ea8
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-04-10 10:25:35 -0300

    feat: create multi select component

---
+++ src/components/MultiSelect/index.tsx
@@ -0,0 +1,21 @@
+import { MultiSelect as MultiSelectComponent } from "react-native-element-dropdown";
+import { MultiSelectProps } from "react-native-element-dropdown/lib/typescript/components/MultiSelect/model";
+
+import { commonStyles } from "../common-styles";
+import { styles } from "./styles";
+
+export function MultiSelect<T>(props: MultiSelectProps<T>) {
+  return (
+    <MultiSelectComponent
+      style={commonStyles.input}
+      placeholderStyle={commonStyles.inputPlaceholderLabel}
+      itemTextStyle={commonStyles.inputContentLabel}
+      selectedStyle={styles.selectedStyle}
+      selectedTextStyle={commonStyles.inputContentLabel}
+      iconStyle={commonStyles.inputIconImageType}
+      inside
+      mode="modal"
+      {...props}
+    />
+  );
+}
