91657c1127dbf61b54836053795cd97436b6b995
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-05-20 18:53:27 -0300

    feat: implement stable version validation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,46 @@
 import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { compare } from "compare-versions";
+import * as Application from "expo-application";
+import { useEffect, useState } from "react";
+import { Platform } from "react-native";
 
 import { Splash } from "@/components/Splash";
 import { useAuth } from "@/contexts/AuthContext";
+import { AppUpdate } from "@/pages/AppUpdate";
 import { CreateOccasionalMaintenance } from "@/pages/CreateOccasionalMaintenance";
 import { ForgotPassword } from "@/pages/ForgotPassword";
 import { Login } from "@/pages/Login";
 import { MaintenanceDetails } from "@/pages/MaintenanceDetails";
 import { Maintenances } from "@/pages/Maintenances";
+import { getAppStableVersion } from "@/services/getAppStableVersion";
 
 const Stack = createNativeStackNavigator();
 
 export const Routes = () => {
   const { isAuthenticated } = useAuth();
 
-  if (isAuthenticated === undefined) {
+  const [isStableVersion, setIsStableVersion] = useState<boolean>();
+
+  useEffect(() => {
+    const handleGetIsStableVersion = async () => {
+      try {
+        const isStableVersion = await getIsStableVersion();
+        setIsStableVersion(isStableVersion);
+      } catch {
+        setIsStableVersion(true);
+      }
+    };
+
+    handleGetIsStableVersion();
+  }, []);
+
+  if (isAuthenticated === undefined || isStableVersion === undefined) {
     return <Splash />;
+  }
+
+  if (!isStableVersion) {
+    return <AppUpdate />;
   }
 
   if (!isAuthenticated) {
@@ -55,4 +80,20 @@ export const Routes = () => {
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+const getIsStableVersion = async () => {
+  if (__DEV__ || Application.nativeBuildVersion === null) {
+    return true;
+  }
+
+  const lastStableVersion = await getAppStableVersion();
+
+  if (!lastStableVersion) {
+    return true;
+  }
+
+  return Platform.OS === "android"
+    ? parseInt(Application.nativeBuildVersion) >= lastStableVersion.android
+    : compare(Application.nativeBuildVersion, lastStableVersion.ios, ">=");
 };
