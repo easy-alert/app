68ed82d44fd45c21bdc9afc6aff4c9ddb7b00288
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-05-27 11:30:27 -0300

    feat: improve offline badges displaying in the whole app when offline

--- src/pages/Maintenances/OfflineQueueBadge/index.tsx
+++ src/pages/Maintenances/OfflineQueueBadge/index.tsx
@@ -1,32 +1,26 @@
 import React from "react";
 import { ActivityIndicator, Text, View } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
 
 import { useOfflineQueue } from "@/contexts/OfflineQueueContext";
 
-import { createStyle } from "./styles";
+import { styles } from "./styles";
 
 export const OfflineQueueBadge = () => {
-  const { hasInternetConnection, offlineQueueLength, isSyncing } = useOfflineQueue();
+  const { offlineQueueLength, isSyncing } = useOfflineQueue();
 
-  if (hasInternetConnection && offlineQueueLength === 0) {
+  if (!isSyncing) {
     return null;
   }
 
-  const styles = createStyle(isSyncing);
-
   return (
     <View style={styles.container}>
-      {isSyncing && <ActivityIndicator color="#fff" />}
-      {!isSyncing && <Icon name={"cloud-offline-outline"} size={16} color="#fff" />}
+      <ActivityIndicator color="#fff" />
 
-      <Text style={styles.label}>{isSyncing ? "Sincronizando" : "Offline"}</Text>
+      <Text style={styles.label}>{"Sincronizando"}</Text>
 
-      {offlineQueueLength > 0 && (
-        <View style={styles.countContainer}>
-          <Text style={styles.countLabel}>{offlineQueueLength}</Text>
-        </View>
-      )}
+      <View style={styles.countContainer}>
+        <Text style={styles.countLabel}>{offlineQueueLength}</Text>
+      </View>
     </View>
   );
 };
