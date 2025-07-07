0b984eab59308ca815f76ed3b587e0e893bf0fff
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-07 10:50:03 -0300

    fix: rename Filter interface to KanbanFilter and add optional properties for priority names and types

--- src/types/utils/Filter.ts
+++ src/types/utils/Filter.ts
@@ -1,9 +1,11 @@
-export interface Filter {
+export interface KanbanFilter {
   selectedBuildings: string[];
   search: string;
   selectedUsers: string[];
   selectedStatus: string[];
   selectedCategories: string[];
+  selectedPriorityNames?: string[];
+  selectedTypes?: string[];
   startDate: string;
   endDate: string;
 }
