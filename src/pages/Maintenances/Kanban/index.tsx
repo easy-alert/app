11b1baaaba5dbe52d9e10cf5324ebbd6e430b40b
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-16 11:04:59 -0300

    feat: updates Kanban board with dynamic counts

--- src/pages/Maintenances/Kanban/index.tsx
+++ src/pages/Maintenances/Kanban/index.tsx
@@ -4,9 +4,14 @@ import Checkbox from "expo-checkbox";
 
 import { useOfflineQueue } from "@/contexts/OfflineQueueContext";
 
+import { LabelInput } from "@/components/LabelInput";
+
+import { handleTranslate } from "@/utils/handleTranslate";
+
 import type { IKanbanColumn } from "@/types/api/IKanbanColumn";
 import type { AvailableFilter } from "@/types/utils/AvailableFilter";
 import type { KanbanFilter } from "@/types/utils/Filter";
+import type { IMaintenancesLength } from "@/types/utils/IMaintenancesLength";
 
 import { KanbanHeader } from "../KanbanHeader";
 import { KanbanRow } from "../KanbanRow";
@@ -16,14 +21,33 @@ interface KanbanProps {
   kanbanData: IKanbanColumn[];
   filters: KanbanFilter;
   availableCategories: AvailableFilter[];
+  maintenancesLength: IMaintenancesLength;
   setFilters: (filters: KanbanFilter) => void;
+  handleChangeMaintenancesLength: (key: string, value: number) => void;
 }
 
-export const Kanban = ({ kanbanData, filters, setFilters, availableCategories }: KanbanProps) => {
+export const Kanban = ({
+  kanbanData,
+  filters,
+  availableCategories,
+  maintenancesLength,
+  setFilters,
+  handleChangeMaintenancesLength,
+}: KanbanProps) => {
   const { offlineQueue } = useOfflineQueue();
 
   const [showOldExpired, setShowOldExpired] = useState(false);
   const [showFuture, setShowFuture] = useState(false);
+
+  const handleFutureCheckboxChange = (value: boolean) => {
+    setShowFuture(value);
+    handleChangeMaintenancesLength(
+      "pending",
+      value
+        ? maintenancesLength.pending + maintenancesLength.future
+        : maintenancesLength.pending - maintenancesLength.future,
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -33,30 +57,30 @@ export const Kanban = ({ kanbanData, filters, setFilters, availableCategories }:
         {kanbanData?.map((column, index) => (
           <View key={index} style={styles.statusContainer}>
             <View style={styles.statusHeader}>
-              <Text style={styles.statusTitle}>{`${column.status} (${column.maintenances.length})`}</Text>
+              <Text
+                style={styles.statusTitle}
+              >{`${column.status === "Vencidas" ? "Vencidas/Expiradas" : column.status} (${maintenancesLength[handleTranslate(column.status) as keyof IMaintenancesLength]})`}</Text>
 
               {column.status === "Vencidas" && (
-                <View style={styles.checkboxContainer}>
+                <LabelInput
+                  label=""
+                  containerOnTouchEnd={() => setShowOldExpired(!showOldExpired)}
+                  style={styles.checkboxContainer}
+                >
                   <Text style={styles.checkboxText}>Mostrar expiradas</Text>
-                  <Checkbox
-                    style={styles.checkbox}
-                    color={showOldExpired ? "#FF0000" : undefined}
-                    value={showOldExpired}
-                    onValueChange={setShowOldExpired}
-                  />
-                </View>
+                  <Checkbox value={showOldExpired} color={showOldExpired ? "#ff3535" : undefined} />
+                </LabelInput>
               )}
 
               {column.status === "Pendentes" && (
-                <View style={styles.checkboxContainer}>
+                <LabelInput
+                  label=""
+                  containerOnTouchEnd={() => handleFutureCheckboxChange(!showFuture)}
+                  style={styles.checkboxContainer}
+                >
                   <Text style={styles.checkboxText}>Mostrar futuras</Text>
-                  <Checkbox
-                    style={styles.checkbox}
-                    color={showFuture ? "#FEA628" : undefined}
-                    value={showFuture}
-                    onValueChange={setShowFuture}
-                  />
-                </View>
+                  <Checkbox value={showFuture} color={showFuture ? "#FEA628" : undefined} />
+                </LabelInput>
               )}
             </View>
 
