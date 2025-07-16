8dec78fc29ff6c79ba899228953472ae9910a2ce
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-16 17:49:42 -0300

    feat: extends IAuthUser interface

--- src/types/api/IAuthUser.ts
+++ src/types/api/IAuthUser.ts
@@ -1,13 +1,27 @@
+import type { IPushNotification } from "./IPushNotification";
 import type { IUserBuildingPermission } from "./IUserBuildingPermission";
 import type { IUserPermission } from "./IUserPermission";
 
 export interface IAuthUser {
   id: string;
+
   name: string;
   email: string;
+  emailIsConfirmed: boolean;
   phoneNumber: string;
+  phoneNumberIsConfirmed: boolean;
+
+  role: string;
+  image: string;
+  colorScheme: string;
+
   isBlocked: boolean;
   isCompanyOwner: boolean;
+
+  PushNotification: IPushNotification[];
   Permissions: { Permission: IUserPermission }[];
   UserBuildingsPermissions: IUserBuildingPermission[];
+
+  lastAccess: string;
+  createdAt: string;
 }
