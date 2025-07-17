38e6b96c265fa1c293a6180a543fcb0f6e66a104
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-17 18:38:13 -0300

    feat: create ShareMaintenance component for maintenance details

---
+++ src/pages/MaintenanceDetails/ShareMaintenance/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    gap: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  textArea: {
+    height: "auto",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 12,
+  },
+});
