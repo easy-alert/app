da5228e6eee3580de485230d8490bc1d68fe4419
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-17 18:04:54 -0300

    feat: LoginCompanySelection to navigation routes

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,14 +3,23 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { ForgotPassword } from "@/pages/ForgotPassword";
 import { Login } from "@/pages/Login";
+import { LoginCompanySelection } from "@/pages/LoginCompanySelection";
 
 const Stack = createNativeStackNavigator();
 
 export const AuthRoutes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ headerShown: false, animation: "slide_from_right" }}>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="LoginCompanySelection"
+          component={LoginCompanySelection}
+          options={{
+            headerShown: true,
+            title: "Selecione a empresa",
+          }}
+        />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       </Stack.Navigator>
     </NavigationContainer>
