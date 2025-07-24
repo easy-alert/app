dfcc7d907093a55c87fe478bdd7866209392cb75
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-24 14:18:37 -0300

    feat: add update report button and logics

--- src/pages/MaintenanceDetails/CallToActions/index.tsx
+++ src/pages/MaintenanceDetails/CallToActions/index.tsx
@@ -13,6 +13,7 @@ import type { ProtectedNavigation } from "@/routes/navigation";
 import { updateMaintenance } from "@/services/mutations/updateMaintenance";
 import { updateMaintenanceFinish } from "@/services/mutations/updateMaintenanceFinish";
 import { updateMaintenanceProgress } from "@/services/mutations/updateMaintenanceProgress";
+import { updateMaintenanceReport } from "@/services/mutations/updateMaintenanceReport";
 import { uploadFile } from "@/services/mutations/uploadFile";
 
 import { alerts } from "@/utils/alerts";
@@ -32,10 +33,9 @@ interface CallToActionsProps {
   remoteFiles: IRemoteFile[];
   remoteImages: IRemoteFile[];
   cost: string;
-  isEditable?: boolean;
-  isEditingReport: boolean;
   setLoading: (loading: boolean) => void;
-  openEditMaintenanceReport: () => void;
+  isFinished: boolean;
+  isEditingReport: boolean;
   handleChangeEditingReport: (state: boolean) => void;
 }
 
@@ -46,10 +46,9 @@ export const CallToActions = ({
   remoteFiles,
   remoteImages,
   cost,
-  isEditable = false,
-  isEditingReport,
   setLoading,
-  openEditMaintenanceReport,
+  isFinished,
+  isEditingReport,
   handleChangeEditingReport,
 }: CallToActionsProps) => {
   const navigation = useNavigation<ProtectedNavigation>();
@@ -281,6 +280,102 @@ export const CallToActions = ({
     setLoading(false);
   };
 
+  const handleUpdateMaintenanceReport = async () => {
+    setLoading(true);
+
+    const formatedCost = convertCostToInteger(cost);
+
+    const networkState = await NetInfo.fetch();
+    const isConnected = networkState.isConnected;
+
+    if (isConnected) {
+      const filesUploaded: IRemoteFile[] = [];
+
+      const uploadFilesPromises = localFiles.map(async (file) => {
+        const { success, data } = await uploadFile({
+          uri: file.uri,
+          type: file.type,
+          name: file.name,
+        });
+
+        if (!success) {
+          return;
+        }
+
+        filesUploaded.push({
+          name: file.name,
+          url: data.url,
+        });
+      });
+
+      const imagesUploaded: IRemoteFile[] = [];
+
+      const uploadImagesPromises = localImages.map(async (image) => {
+        const { success, data } = await uploadFile({
+          uri: image.uri,
+          type: image.type,
+          name: image.name,
+        });
+
+        if (!success) {
+          return;
+        }
+
+        imagesUploaded.push({
+          name: image.name,
+          url: data.url,
+        });
+      });
+
+      await Promise.all([...uploadFilesPromises, ...uploadImagesPromises]);
+
+      const { success, message } = await updateMaintenanceReport({
+        maintenanceHistoryId: maintenanceDetails.id,
+        maintenanceReport: {
+          id: maintenanceDetails.MaintenanceReport?.[0].id || "",
+          cost: formatedCost,
+          observation: "",
+        },
+        files: [...filesUploaded, ...remoteFiles].map((file) => ({
+          originalName: file.name,
+          name: file.name,
+          url: file.url,
+        })),
+        images: [...imagesUploaded, ...remoteImages].map((image) => ({
+          originalName: image.name,
+          name: image.name,
+          url: image.url,
+        })),
+      });
+
+      if (success) {
+        toast.success(message);
+        navigation.goBack();
+      } else {
+        alerts.error(message);
+      }
+    } else {
+      const newEntry: OfflineQueueItem = {
+        type: "updateMaintenanceReport",
+        userId,
+        maintenanceId: maintenanceDetails.id,
+        maintenanceReportId: maintenanceDetails.MaintenanceReport?.[0].id || "",
+        cost: formatedCost,
+        localFiles,
+        localImages,
+        remoteFiles,
+        remoteImages,
+      };
+
+      await addItem(newEntry);
+      navigation.goBack();
+    }
+
+    setLoading(false);
+
+    handleChangeEditingReport(!isEditingReport);
+  };
+
   const openFinishMaintenanceAlert = () => {
     Alert.alert("Confirmar Ação", "Tem certeza de que deseja finalizar a manutenção?", [
       {
@@ -294,13 +389,39 @@ export const CallToActions = ({
     ]);
   };
 
+  const openUpdateMaintenanceReportAlert = () => {
+    Alert.alert("Confirmar Ação", "Tem certeza de que deseja atualizar o relatório?", [
+      {
+        text: "Cancelar",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: handleUpdateMaintenanceReport,
+      },
+    ]);
+  };
+
   return (
-    <View style={[styles.container, isEditable && { justifyContent: "center" }]}>
-      {isEditable ? (
-        <PrimaryButton
-          label={isEditingReport ? "Atualizar relato" : "Editar relato"}
-          onPress={() => handleChangeEditingReport(true)}
-        />
+    <View style={[styles.container, isFinished && !isEditingReport && { justifyContent: "center" }]}>
+      {isFinished ? (
+        <>
+          {isEditingReport && (
+            <SecondaryButton label="Voltar" onPress={() => handleChangeEditingReport(!isEditingReport)} />
+          )}
+
+          <PrimaryButton
+            label={isEditingReport ? "Atualizar relatório" : "Editar relatório"}
+            onPress={() => {
+              if (isEditingReport) {
+                openUpdateMaintenanceReportAlert();
+              } else {
+                handleChangeEditingReport(!isEditingReport);
+                Alert.alert("Editar relatório", "A seguir você conseguira editar o relatório da manutenção.");
+              }
+            }}
+          />
+        </>
       ) : (
         <>
           <SecondaryButton
